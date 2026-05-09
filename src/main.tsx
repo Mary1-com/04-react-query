bdbf65e52188ee6f1ac311a65424fac38bc6efc0
Mary1_com <vnnkvmrn76@gmail.com>
2026-05-09 11:42:23 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+
+
+// import "normalize";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
